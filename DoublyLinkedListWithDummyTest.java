15a4e7571bae76feea732f6bd6eca1f0c8399893
akwaed <akwaed@gmail.com>
2024-02-29 01:39:18 -0500

    updated testcase

--- DoublyLinkedListWithDummyTest.java
+++ DoublyLinkedListWithDummyTest.java
@@ -43,5 +43,129 @@ public class DoublyLinkedListWithDummyTest {
   public void testGet(){
     assertEquals(Integer.valueOf(2), filledList.get(2));
   }
+@Test
+    public void testSize() {
+        assertEquals(0, emptyList.size());
+        assertEquals(5, filledList.size());
+    }
+
+    @Test
+    public void testIsEmpty() {
+        assertTrue(emptyList.isEmpty());
+        assertFalse(filledList.isEmpty());
+    }
+
+    @Test
+    public void testClear() {
+        filledList.clear();
+        assertTrue(filledList.isEmpty());
+    }
+
+    @Test
+    public void testGetFirst() {
+        assertNull(emptyList.getFirst());
+        assertEquals(Integer.valueOf(0), filledList.getFirst());
+    }
+
+    @Test
+    public void testGetLast() {
+        assertNull(emptyList.getLast());
+        assertEquals(Integer.valueOf(4), filledList.getLast());
+    }
+
+    @Test
+    public void testGet() {
+        assertNull(emptyList.get(2));
+        assertEquals(Integer.valueOf(2), filledList.get(2));
+    }
+
+    @Test
+    public void testSet() {
+        assertNull(emptyList.set(2, 10));
+        assertEquals(Integer.valueOf(2), filledList.set(2, 10));
+        assertEquals(Integer.valueOf(10), filledList.get(2));
+    }
+
+    @Test
+    public void testAddFirst() {
+        emptyList.addFirst(10);
+        assertEquals("[10]", emptyList.toString());
+        assertEquals(Integer.valueOf(10), emptyList.getFirst());
+        assertEquals(1, emptyList.size());
+    }
+
+    @Test
+    public void testAdd() {
+        filledList.add(2, 10);
+        assertEquals("[0, 1, 10, 2, 3, 4]", filledList.toString());
+        assertEquals(Integer.valueOf(10), filledList.get(2));
+        assertEquals(6, filledList.size());
+    }
+
+    @Test
+    public void testAddLast() {
+        emptyList.addLast(10);
+        assertEquals("[10]", emptyList.toString());
+        assertEquals(Integer.valueOf(10), emptyList.getLast());
+        assertEquals(1, emptyList.size());
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("[]", emptyList.toString());
+        assertEquals("[0, 1, 2, 3, 4]", filledList.toString());
+    }
+
+    @Test
+    public void testToStringReverse() {
+        assertEquals("[]", emptyList.toStringReverse());
+        assertEquals("[4, 3, 2, 1, 0]", filledList.toStringReverse());
+    }
+
+    @Test
+    public void testRemoveFirst() {
+        assertEquals(Integer.valueOf(0), filledList.removeFirst());
+        assertEquals("[1, 2, 3, 4]", filledList.toString());
+        assertEquals(4, filledList.size());
+    }
+
+    @Test
+    public void testRemove() {
+        assertEquals(Integer.valueOf(2), filledList.remove(2));
+        assertEquals("[0, 1, 3, 4]", filledList.toString());
+        assertEquals(4, filledList.size());
+    }
+
+    @Test
+    public void testRemoveLast() {
+        assertEquals(Integer.valueOf(4), filledList.removeLast());
+        assertEquals("[0, 1, 2, 3]", filledList.toString());
+        assertEquals(4, filledList.size());
+    }
+
+    @Test
+    public void testRemoveWithoutIndex() {
+        assertEquals(Integer.valueOf(0), filledList.remove());
+        assertEquals("[1, 2, 3, 4]", filledList.toString());
+        assertEquals(4, filledList.size());
+    }
+
+    @Test
+    public void testLastIndexOf() {
+        assertEquals(2, filledList.lastIndexOf(2));
+        assertEquals(-1, filledList.lastIndexOf(10));
+    }
+
+    @Test
+    public void testIndexOf() {
+        assertEquals(2, filledList.indexOf(2));
+        assertEquals(-1, filledList.indexOf(10));
+    }
+
+    @Test
+    public void testContains() {
+        assertTrue(filledList.contains(2));
+        assertFalse(filledList.contains(10));
+    }
 }
 
